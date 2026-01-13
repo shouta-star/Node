fdc48bfa9522163729e40d7c6e71eea672c314ff
shota fukano <78405961+shouta-star@users.noreply.github.com>
2026-01-13 17:27:56 +0900

    調整

--- Assets/Scripts/EvaluationLogger.cs
+++ Assets/Scripts/EvaluationLogger.cs
@@ -125,7 +125,7 @@ public static class EvaluationLogger
             //string baseDir = @"D:\GitHub\Node\CSV\Farthest_OnArrival_NewestNode";
             //string baseDir = @"D:\GitHub\Node\CSV\MostUnknown_OnArrival_FarthestFromStart";
             //string baseDir = @"D:\GitHub\Node\CSV\Test";
-            string baseDir = @"D:\NodeGitHub\Node\CSV\0113";
+            string baseDir = @"D:\GitHub\NodeGitHub\CSV\Test";
             if (!Directory.Exists(baseDir))
             {
                 Directory.CreateDirectory(baseDir);
@@ -268,7 +268,7 @@ public static class EvaluationLogger
             //string baseDir = @"D:\GitHub\Node\CSV\Farthest_OnArrival_NewestNode";
             //string baseDir = @"D:\GitHub\Node\CSV\MostUnknown_OnArrival_FarthestFromStart";
             //string baseDir = @"D:\GitHub\Node\CSV\Test";
-            string baseDir = @"D:\NodeGitHub\Node\CSV\0113";
+            string baseDir = @"D:\GitHub\NodeGitHub\CSV\Test";
             if (!Directory.Exists(baseDir))
             {
                 Directory.CreateDirectory(baseDir);
